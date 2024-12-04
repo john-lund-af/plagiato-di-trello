944337ed4809eb66014f70e2dcb7b162fba57d1e
John Lundgren <john.lundgren.lexicon@gmail.com>
2024-12-04 19:26:28 +0100

    Installation and configuration for testing.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import Link from 'next/link'
 
 export default function Home() {
-  return <div>Home Page</div>
+  return (<div>
+    <h1>Home</h1>
+    <Link href="/about">About</Link>
+  </div>)
 }
